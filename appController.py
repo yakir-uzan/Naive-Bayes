472e8b49db578de0dcb66c8265c825d2d8834886
Yakir Uzan <yakiruzangreen@gmail.com>
2025-07-22 09:05:31 +0300

    added app control

--- appController.py
+++ appController.py
@@ -1,14 +1,17 @@
 from naiveBayesModel import NaiveBayesModel
 from loader import DatasetLoader
 from naiveBayesClassifier import NaiveBayesClassifier
+from dataCleaner import DataCleaner
 
 class AppController:
     def __init__(self):
         self.loader = DatasetLoader()
+        self.cleaner = DataCleaner()
         self.model = NaiveBayesModel()
         self.classifier = None
 
     def run(self):
         train_df = self.loader.load("DB.csv")
+        self.cleaner.clean("DB.csv")
         self.model.fit(train_df)
         self.classifier = NaiveBayesClassifier(self.model)
